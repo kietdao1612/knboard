4a0002fe5df456db55ccbc43eba963d7a81c64cb
raahmed <raahmed@microsoft.com>
2020-08-21 12:02:33 -0700

    Using secure ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,8 +6,8 @@ DEBUG = False
 SECRET_KEY = get_env("DJANGO_SECRET_KEY")
 STATIC_URL = get_env("DJANGO_STATIC_URL")
 STATIC_ROOT = get_env("DJANGO_STATIC_ROOT")
-#ALLOWED_HOSTS = get_env("DJANGO_ALLOWED_HOSTS").split(",")
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = get_env("DJANGO_ALLOWED_HOSTS").split(",")
+
 # Nginx is used instead of SecurityMiddleware
 # for setting all the recommended security headers
 SILENCED_SYSTEM_CHECKS = [
