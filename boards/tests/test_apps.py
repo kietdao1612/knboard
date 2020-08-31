1e32c0ba1bea0872652ade9fd1c23f446fc8a464
raahmed <raahmed@microsoft.com>
2020-08-31 15:20:24 -0700

    Moving closer to original repo structure.

--- boards/tests/test_apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import apps
-
-from boards.apps import BoardsConfig
-
-
-def test_config():
-    assert BoardsConfig.name == "boards"
-    assert apps.get_app_config("boards").name == "boards"
