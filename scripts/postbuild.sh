a1df2d7154ec2519404ccf4ecdf9e1725ad854fc
Rabeea Emad <raahmed@microsoft.com>
2020-08-31 16:39:13 -0700

    Update postbuild.sh

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -3,5 +3,5 @@ python manage.py migrate
 python manage.py loaddata avatars
 python manage.py runserver &
 
-#cd /home/site/wwwroot/frontend
-#yarn install
+cd /home/site/wwwroot/frontend
+yarn install
