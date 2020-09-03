b949b336b7791cccd9682c84586f2ccc772a190e
raahmed <raahmed@microsoft.com>
2020-09-03 11:12:14 -0700

    Changing order so that build folder is made before collect static is run.

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,8 +1,8 @@
-cd ./backend
+cd ./frontend
+yarn install
+yarn build
+
+cd ../backend
 python manage.py migrate
 python manage.py loaddata avatars
 python manage.py collectstatic
-
-cd ../frontend
-yarn install
-yarn build
