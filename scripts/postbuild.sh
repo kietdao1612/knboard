7e775737c6013f713859875cd6f6f7dba9863f5f
Rabeea Emad <raahmed@microsoft.com>
2020-11-03 13:20:17 -0800

    Update postbuild.sh

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,8 +1,6 @@
-cd ../frontend
-#yarn install
-#yarn build
+cd ./backend
+python manage.py collectstatic --no-input
 
-#cd ../backend
-#python manage.py migrate
-#python manage.py loaddata avatars
-# change
+cd ../frontend
+yarn install
+yarn build
